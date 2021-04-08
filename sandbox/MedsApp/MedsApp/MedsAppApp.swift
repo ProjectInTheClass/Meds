4c02b93535da6cc0c70b6090bd9ebb3a61bde2d4
Kim Yong Seong <yongseong97@naver.com>
2021-04-08 18:28:34 +0900

    sandbox

---
+++ sandbox/MedsApp/MedsApp/MedsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedsAppApp.swift
+//  MedsApp
+//
+//  Created by Yong Seong Kim on 2021/04/08.
+//
+
+import SwiftUI
+
+@main
+struct MedsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
