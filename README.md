3cb7f523984cb59592e0a4458dd69f6f6f6b78da
Kim Yong Seong <yongseong97@naver.com>
2021-04-30 16:59:19 +0900

    test

--- README.md
+++ README.md
@@ -1,6 +1,23 @@
-# ⏰자취생들을 위한 재료관리 어플 , Meds(Management Expiration Dates)
+# ⏰ 자취생들을 위한 재료관리 어플 , Meds(Management Expiration Dates)
 
 ### 개발환경
 - IDE : Xcode
 - Language : Swift
+
+한양대학교 소프트웨어 스튜디오 수업의 일환으로 만들어진 IOS APP 입니다.
+
+음식을 사놓고 유통기한을 몰라 버리는 경우가 많으신가요? 걱정하지 않으셔도 됩니다. 이젠 Meds가 도와드리겠습니다.
+
+주요 기능
+
+- 바코드를 통해 재료의 유통기한을 관리
+
+- 유통기한 3일전 알림.
+
+- 재료에 대한 간단한 음식 추천.
+
+![Introduction](./introduction.jpeg)
+
+
+
 
--- app/MedsApp/MedsApp/Barcode.swift
+++ app/MedsApp/MedsApp/Barcode.swift
@@ -10,141 +10,174 @@ import AVFoundation
 
 class Barcode: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
     
-//        var captureSession: AVCaptureSession!
-//        var previewLayer: AVCaptureVideoPreviewLayer!
-    var captureSession:AVCaptureSession?
+    
+    var captureSession = AVCaptureSession()
     var videoPreviewLayer:AVCaptureVideoPreviewLayer?
     var qrCodeFrameView:UIView?
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // Get the back-facing camera for capturing videos
-        let deviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInDualCamera], mediaType: AVMediaType.video, position: .back)
-        
-        //AVMediaType.video를 지원하는 세션을 찾음.
-        
-        
-        do {
-            // Get an instance of the AVCaptureDeviceInput class using the previous device object.
-            let input = try AVCaptureDeviceInput(device: captureDevice)
-            
-            // Set the input device on the capture session.
-            captureSession.addInput(input)
-            
-            // Initialize a AVCaptureMetadataOutput object and set it as the output device to the capture session.
-            let captureMetadataOutput = AVCaptureMetadataOutput()
-            captureSession.addOutput(captureMetadataOutput)
-            
-            // Set delegate and use the default dispatch queue to execute the call back
-            captureMetadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
-            captureMetadataOutput.metadataObjectTypes = [AVMetadataObject.ObjectType.qr]
-        } catch {
-            // If any error occurs, simply print it out and don't continue any more.
-            print(error)
-            return
-        }
+    private let supportedCodeTypes = [AVMetadataObject.ObjectType.upce,
+                                          AVMetadataObject.ObjectType.code39,
+                                          AVMetadataObject.ObjectType.code39Mod43,
+                                          AVMetadataObject.ObjectType.code93,
+                                          AVMetadataObject.ObjectType.code128,
+                                          AVMetadataObject.ObjectType.ean8,
+                                          AVMetadataObject.ObjectType.ean13,
+                                          AVMetadataObject.ObjectType.aztec,
+                                          AVMetadataObject.ObjectType.pdf417,
+                                          AVMetadataObject.ObjectType.itf14,
+                                          AVMetadataObject.ObjectType.dataMatrix,
+                                          AVMetadataObject.ObjectType.interleaved2of5,
+                                          AVMetadataObject.ObjectType.qr]
 
-    }
-//        override func viewDidLoad() {
-//            super.viewDidLoad()
-//
-//            view.backgroundColor = UIColor.black
-//            captureSession = AVCaptureSession()
-//
-//            guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else { return }
-//            let videoInput: AVCaptureDeviceInput
-//
-//            do {
-//                videoInput = try AVCaptureDeviceInput(device: videoCaptureDevice)
-//            } catch {
-//                return
-//            }
-//
-//            if (captureSession.canAddInput(videoInput)) {
-//                captureSession.addInput(videoInput)
-//            } else {
-//                failed()
-//                return
-//            }
-//
-//            let metadataOutput = AVCaptureMetadataOutput()
-//
-//            if (captureSession.canAddOutput(metadataOutput)) {
-//                captureSession.addOutput(metadataOutput)
-//
-//                metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
-//                metadataOutput.metadataObjectTypes = [.ean8, .ean13, .pdf417]
-//            } else {
-//                failed()
-//                return
-//            }
-//
-//            previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-//            previewLayer.frame = view.layer.bounds
-//            previewLayer.videoGravity = .resizeAspectFill
-//            view.layer.addSublayer(previewLayer)
-//
-//            captureSession.startRunning()
-//        }
-//
-//        func failed() {
-//            let ac = UIAlertController(title: "Scanning not supported", message: "Your device does not support scanning a code from an item. Please use a device with a camera.", preferredStyle: .alert)
-//            ac.addAction(UIAlertAction(title: "OK", style: .default))
-//            present(ac, animated: true)
-//            captureSession = nil
-//        }
-//
-//        override func viewWillAppear(_ animated: Bool) {
-//            super.viewWillAppear(animated)
-//
-//            if (captureSession?.isRunning == false) {
-//                captureSession.startRunning()
-//            }
-//        }
-//
-//        override func viewWillDisappear(_ animated: Bool) {
-//            super.viewWillDisappear(animated)
-//
-//            if (captureSession?.isRunning == true) {
-//                captureSession.stopRunning()
-//            }
-//        }
-//
-//        func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
-//            captureSession.stopRunning()
-//
-//            if let metadataObject = metadataObjects.first {
-//                guard let readableObject = metadataObject as? AVMetadataMachineReadableCodeObject else { return }
-//                guard let stringValue = readableObject.stringValue else { return }
-//                AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
-//                found(code: stringValue)
-//            }
-//
-//            dismiss(animated: true)
-//        }
-//
-//        func found(code: String) {
-//            print(code)
-//        }
-//
-//        override var prefersStatusBarHidden: Bool {
-//            return true
-//        }
-//
-//        override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-//            return .portrait
-//        }
+override func viewDidLoad() {
+    super.viewDidLoad()
     
+guard let captureDevice = AVCaptureDevice.default(for: AVMediaType.video) else {
+           print("Failed to get the camera device")
+           return
+       }
+       
+       do {
+           // Get an instance of the AVCaptureDeviceInput class using the previous device object.
+           let input = try AVCaptureDeviceInput(device: captureDevice)
+           
+           // Set the input device on the capture session.
+           captureSession.addInput(input)
+           
+           // Initialize a AVCaptureMetadataOutput object and set it as the output device to the capture session.
+           let captureMetadataOutput = AVCaptureMetadataOutput()
+           captureSession.addOutput(captureMetadataOutput)
+           
+           // Set delegate and use the default dispatch queue to execute the call back
+           captureMetadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+//                   captureMetadataOutput.metadataObjectTypes = supportedCodeTypes
+           captureMetadataOutput.metadataObjectTypes = supportedCodeTypes
+        
+        
+           
+       } catch {
+           // If any error occurs, simply print it out and don't continue any more.
+           print(error)
+           return
+       }
+       
+       // Initialize the video preview layer and add it as a sublayer to the viewPreview view's layer.
+       videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+       videoPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
+       videoPreviewLayer?.frame = view.layer.bounds
+       view.layer.addSublayer(videoPreviewLayer!)
+       
+       // Start video capture.
+       captureSession.startRunning()
 
-    /*
-    // MARK: - Navigation
+        // Move the message label and top bar to the front
+        view.bringSubview(toFront: messageLabel)
+        view.bringSubview(toFront: topbar)
+        // Initialize QR Code Frame to highlight the QR code
+        qrCodeFrameView = UIView()
+        
+        if let qrCodeFrameView = qrCodeFrameView {
+            qrCodeFrameView.layer.borderColor = UIColor.green.cgColor
+            qrCodeFrameView.layer.borderWidth = 2
+            view.addSubview(qrCodeFrameView)
+            view.bringSubview(toFront: qrCodeFrameView)
+        }
+    
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+
+
+override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+}
+    
+func launchApp(decodedURL: String) {
+   
+   if presentedViewController != nil {
+       return
+   }
+   
+   let alertPrompt = UIAlertController(title: "Open App", message: "You're going to open \(decodedURL)", preferredStyle: .actionSheet)//창을 띄어줌.
+
+   let confirmAction = UIAlertAction(title: "Confirm", style: UIAlertAction.Style.default, handler: { (action) -> Void in
+       
+       if let url = URL(string: decodedURL) {
+           if UIApplication.shared.canOpenURL(url) {
+               UIApplication.shared.open(url)
+           }
+       }
+   })
+   
+   let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertAction.Style.cancel, handler: nil)
+   
+   alertPrompt.addAction(confirmAction)
+   alertPrompt.addAction(cancelAction)
+   
+   present(alertPrompt, animated: true, completion: nil)
+}
+    
+    
+ private func updatePreviewLayer(layer: AVCaptureConnection, orientation: AVCaptureVideoOrientation) {
+       layer.videoOrientation = orientation
+       videoPreviewLayer?.frame = self.view.bounds
+ }
+     
+ override func viewDidLayoutSubviews() {
+   super.viewDidLayoutSubviews()
+   
+   if let connection =  self.videoPreviewLayer?.connection
+   {
+     let currentDevice: UIDevice = UIDevice.current
+     let orientation: UIDeviceOrientation = currentDevice.orientation
+     let previewLayerConnection : AVCaptureConnection = connection
+     
+     if previewLayerConnection.isVideoOrientationSupported
+     {
+           switch (orientation) {
+           case .portrait:
+             updatePreviewLayer(layer: previewLayerConnection, orientation: .portrait)
+             break
+           case .landscapeRight:
+             updatePreviewLayer(layer: previewLayerConnection, orientation: .landscapeLeft)
+             break
+           case .landscapeLeft:
+             updatePreviewLayer(layer: previewLayerConnection, orientation: .landscapeRight)
+             break
+           case .portraitUpsideDown:
+             updatePreviewLayer(layer: previewLayerConnection, orientation: .portraitUpsideDown)
+             break
+           default:
+             updatePreviewLayer(layer: previewLayerConnection, orientation: .portrait)
+             break
+       }
+     }
+   }
+ }
+
+
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+       // Check if the metadataObjects array is not nil and it contains at least one object.
+       if metadataObjects.count == 0 {
+           qrCodeFrameView?.frame = CGRect.zero
+           messageLabel.text = "No QR code is detected"
+           return
+       }
+       
+       // Get the metadata object.
+       let metadataObj = metadataObjects[0] as! AVMetadataMachineReadableCodeObject
+       
+    //            print(metadata)
+       if supportedCodeTypes.contains(metadataObj.type) {
+           // If the found metadata is equal to the QR code metadata (or barcode) then update the status label's text and set the bounds
+           let barCodeObject = videoPreviewLayer?.transformedMetadataObject(for: metadataObj)
+           qrCodeFrameView?.frame = barCodeObject!.bounds
+           
+           if metadataObj.stringValue != nil {
+               launchApp(decodedURL: metadataObj.stringValue!)
+               messageLabel.text = metadataObj.stringValue
+           }
+       }
     }
-    */
-
+       
 }
